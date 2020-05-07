5416b2b1e793d6f19c479310efd2ac8a1e322f62
Will Ediger <will.ediger@gmail.com>
2020-05-06 21:47:18 -0500

    file io solution

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -11,9 +11,27 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+with open('src/foo.txt') as foo:
+    read_data = foo.read()
+    print(read_data)
+
+#if using `with`, the file is automatically closed after the block ends.
+assert foo.closed == True, "Should be True"
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+with open('src/bar.txt', 'w') as bar:
+    txt = """line 1
+line 2
+line 3"""
+    bar.write(txt)
+
+with open('src/bar.txt') as bar:
+    assert bar.read() == """line 1
+line 2
+line 3""", "Should match string exactly"
